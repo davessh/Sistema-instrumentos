4fb7437a3c70c619b1fd2802a09a90c9227f7a1c
davessh <david.schlemmer@uabc.edu.mx>
2025-08-20 11:09:58 -0700

    clase instrumento creada, siguiendo el planteamiento de clase y cumpliendo con dicha propuesta

---
+++ src/Control.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Control {
+    ArrayList<Instrumento> instrumentos;
+
+    public Control() {
+    }
+
+    public String consultarTodos(){
+        return "" ;
+    }
+
+    public String consultaPorAutor(String autor){
+
+        return "" ;
+    }
+
+    public String consultaPorEvaluacion(String evaluacion){
+        return "";
+    }
+
+}
