4d7d48ca6a62fd7a06db678f4a1fb28beb6596f0
davessh <david.schlemmer@uabc.edu.mx>
2025-08-20 08:45:11 -0700

    clase instrumento creada, siguiendo el planteamiento de clase y cumpliendo con dicha propuesta

---
+++ src/Instrumento.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+public class Instrumento {
+
+    private int clave;
+    private String nombre;
+    private String autor;
+    private String tipo;
+    private String proposito; //tipo (identificar o manejar)
+    private String tipoCondicion;
+    private boolean evaluacion;
+    private String citaEvaluacion;
+
+    public Instrumento(int clave, String nombre,
+                       String autor, String tipo, String proposito, String tipoCondicion) {
+        this.clave = clave;
+        this.nombre = nombre;
+        this.autor = autor;
+        this.tipo = tipo;
+        this.proposito = proposito;
+        this.tipoCondicion = tipoCondicion;
+        this.evaluacion = false;
+        this.citaEvaluacion = "";
+    }
+
+    public int getClave() {
+        return clave;
+    }
+    public void setClave(int clave) {
+        this.clave = clave;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    public String getProposito() {
+        return proposito;
+    }
+    public void setProposito(String proposito) {
+        this.proposito = proposito;
+    }
+    public String getTipoCondicion() {
+        return tipoCondicion;
+    }
+    public void setTipoCondicion(String tipoCondicion) {
+        this.tipoCondicion = tipoCondicion;
+    }
+    public boolean isEvaluacion() {
+        return evaluacion;
+    }
+    public void setEvaluacion(boolean evaluacion) {
+        this.evaluacion = evaluacion;
+    }
+    public String getCitaEvaluacion() {
+        return citaEvaluacion;
+    }
+    public void setCitaEvaluacion(String citaEvaluacion) {
+        this.citaEvaluacion = citaEvaluacion;
+    }
+
+    public String toString() {
+        return "Autor: " + autor + "\nClave: " + clave + "\nTipo: " + proposito + "\nCondiciones: " + tipoCondicion +
+                "\nEvaluacion: " + evaluacion + "Cita de la evaluacion:" + citaEvaluacion + "\n\n";
+    }
+}
